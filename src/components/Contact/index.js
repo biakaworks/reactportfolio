06562ec3c51884366cb3ef29dc0b4cd6ca27faba
Biaka Z <biaka@biakaworks.com>
2021-02-12 11:43:22 -0600

    Updated Components with Pages

---
+++ src/components/Contact/index.js
@@ -0,0 +1,48 @@
+function Contact() {
+    return (
+      <div className="container" id="main-area">
+        <div className="row">
+          <div className="col-12">Contact</div>
+        </div>
+        {/* <!-- About Me Section and Biography --> */}
+        <div className="row">
+          <div className="col-12 col-lg-12">
+            <form>
+              <div className="form-group">
+                <label for="formGroupExampleInput">Name</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="formGroupExampleInput"
+                  placeholder="Name"
+                />
+              </div>
+              <div className="form-group">
+                <label for="formGroupExampleInput2">Email</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="formGroupExampleInput2"
+                  placeholder="Email"
+                />
+              </div>
+              <div className="form-group">
+                <label for="formGroupExampleInput2">Message</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="formGroupExampleInput2"
+                  placeholder="Message"
+                />
+              </div>
+              <button type="button" className="btn btn-success">
+                Submit
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    );
+  }
+  
+  export default Contact;
---
+++ src/components/Footer/index.js
@@ -0,0 +1,11 @@
+function Footer() {
+  return (
+    <div>
+          <footer class="footer bg-light card-footer text-muted">
+      <p>www.biakaworks.com</p>
+    </footer>  
+    </div>
+  );
+}
+
+export default Footer;
